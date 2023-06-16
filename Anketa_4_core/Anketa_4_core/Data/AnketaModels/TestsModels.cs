903d81479e388af696527dc909d5cab5787f73c6
Иван Князь <knyazivan1992@yandex.ru>
2023-06-16 16:41:49 +0500

    Кеттел

--- Anketa_4_core/Anketa_4_core/Data/AnketaModels/TestsModels.cs
+++ Anketa_4_core/Anketa_4_core/Data/AnketaModels/TestsModels.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Anketa_4_core.Data.AnketaModels
 {
@@ -36,7 +37,7 @@ namespace Anketa_4_core.Data.AnketaModels
         [Key]
         public int ID { get; set; }
         public int YearTraining { get; set; }
-        public int QuestionNumber { get; set; }               
+        public int QuestionNumber { get; set; }
         public string QuestionText { get; set; }
     }
 
@@ -64,8 +65,8 @@ namespace Anketa_4_core.Data.AnketaModels
     {
         [Key]
         public int ID { get; set; }
-        public AccessForTestables AccessID { get; set; }
-        public Comp_Answers AnswerID { get; set; }
+        public AccessForTestables Access { get; set; }
+        public Comp_Answers Answer { get; set; }
         public int MarkAnswer { get; set; }
         public bool IsCorrect { get; set; }
         public DateTime DateTimeAnswer { get; set; }
@@ -76,11 +77,66 @@ namespace Anketa_4_core.Data.AnketaModels
 
     #region Кеттел
 
-    public class KettelMain
+    public class KT_Main
     {
         [Key]
         public int ID { get; set; }
+        public int Years { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateTest { get; set; }
+        public AccessForTestables Acces { get; set; }
+    }
 
+    public class KT_Questions
+    {
+        [Key]
+        public int ID { get; set; }
+        public int QuestionNumber { get; set; }
+        public string QuestionText { get; set; }
+    }
+
+    public class KT_Answers
+    {
+        [Key]
+        public int ID { get; set; }
+        public string AnswerNumber { get; set; }
+        public string AnswerText { get; set; }
+        public KT_Questions Question { get; set; }
+    }
+
+    public class KT_Categories
+    {
+        [Key]
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    public class KT_Levels
+    {
+        [Key]
+        public int ID { get; set; }
+        public KT_Categories Category { get; set; }
+        public int ScoreFrom { get; set; }
+        public int ScoreTo { get; set; }
+        public int Level { get; set; }
+        public string LiterForLevel { get; set; }
+        public string LiterForLevelWordInReport { get; set; }
+    }
+
+    public class KT_Key
+    {
+        [Key]
+        public int ID { get; set; }
+        public KT_Categories Category { get; set; }
+        public KT_Answers Answer { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class TestableAnswers
+    {
+        [Key] 
+        public int ID { get; set; }
+        public KT_Main MainInfo { get; set; }
     }
 
     #endregion
